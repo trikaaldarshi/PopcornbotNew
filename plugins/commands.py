1ebac55311b8d121d12a3b95d0cca031d011b08c
त्रिकालदर्शी <86564011+trikaaldarshi@users.noreply.github.com>
2025-09-22 12:16:05 +0530

    Update commands.py
    Updated ,

--- plugins/commands.py
+++ plugins/commands.py
@@ -140,7 +140,7 @@ async def start(client, message):
                     InlineKeyboardButton('ᴛᴏᴘ sᴇᴀʀᴄʜɪɴɢ ⭐', callback_data="topsearch"),
                     InlineKeyboardButton('ᴜᴘɢʀᴀᴅᴇ 🎟', callback_data="premium_info"),
         ],[
-            InlineKeyboardButton('Read Disclaimer ©', url=f'http://telegram.me/{temp.U_NAME}/dmca')
+            InlineKeyboardButton('Read Disclaimer ©', url=f'http://telegram.me/{temp.U_NAME}/dmca'),
                 ]]
         reply_markup = InlineKeyboardMarkup(buttons)
         current_time = datetime.now(pytz.timezone(TIMEZONE))
