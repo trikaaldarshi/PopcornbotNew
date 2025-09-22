c31a6048c4a3e517caaff659b46788851d15c90e
त्रिकालदर्शी <86564011+trikaaldarshi@users.noreply.github.com>
2025-09-22 07:42:51 +0530

    Update info.py

--- info.py
+++ info.py
@@ -31,7 +31,7 @@ INDEX_CAPTION = bool(environ.get('SAVE_CAPTION', True)) # Save caption db when i
 #Making it false will not save caption in db SO you can save some storage space
 
 
-PICS = (environ.get('PICS', 'https://graph.org/file/56b5deb73f3b132e2bb73.jpg https://graph.org/file/5303692652d91d52180c2.jpg https://graph.org/file/425b6f46efc7c6d64105f.jpg https://graph.org/file/876867e761c6c7a29855b.jpg')).split()  # Sample pic
+PICS = (environ.get('PICS', 'https://graph.org/file/af5db5593d4a1fded98cf-b0c40eb9a45a17905f.png https://graph.org/file/d6cd957f001eb1a5e58ca-374de9f9758ef0ae87.png https://graph.org/file/fe9e89a722f1fe87ff4f3-818cd9b6f2f4b1acd3.png')).split()  # Sample pic
 NOR_IMG = environ.get("NOR_IMG", "https://graph.org/file/e20b5fdaf217252964202.jpg")
 MELCOW_PHOTO = environ.get("MELCOW_PHOTO", "https://graph.org/file/56b5deb73f3b132e2bb73.jpg")
 SPELL_IMG = environ.get("SPELL_IMG", "https://graph.org/file/13702ae26fb05df52667c.jpg")
