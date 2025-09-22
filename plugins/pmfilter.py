6a6693d2a60319d5bef97feab24e9eb08ab6d23a
त्रिकालदर्शी <86564011+trikaaldarshi@users.noreply.github.com>
2025-09-22 11:44:09 +0530

    Update pmfilter.py

--- plugins/pmfilter.py
+++ plugins/pmfilter.py
@@ -1412,6 +1412,8 @@ async def cb_handler(client: Client, query: CallbackQuery):
                 ],[
                     InlineKeyboardButton('ᴛᴏᴘ sᴇᴀʀᴄʜɪɴɢ ⭐', callback_data="topsearch"),
                      InlineKeyboardButton('ᴜᴘɢʀᴀᴅᴇ 🎟', callback_data="premium_info"),
+        ],[
+            InlineKeyboardButton('Read Disclaimer ©', url=f'http://telegram.me/{temp.U_NAME}/dmca')
                 ]]
         reply_markup = InlineKeyboardMarkup(buttons)
         current_time = datetime.now(pytz.timezone(TIMEZONE))
@@ -1479,7 +1481,9 @@ async def cb_handler(client: Client, query: CallbackQuery):
         ],[
             InlineKeyboardButton('ᴅᴏɴᴀᴛɪᴏɴ 💰', callback_data='donation'), 
         ],[
-            InlineKeyboardButton('⇋ ʙᴀᴄᴋ ᴛᴏ ʜᴏᴍᴇ ⇋', callback_data='start')
+            InlineKeyboardButton('⇋ ʙᴀᴄᴋ ᴛᴏ ʜᴏᴍᴇ ⇋', callback_data 
+        ],[
+            InlineKeyboardButton('Read Disclaimer ©', url=f'http://telegram.me/{temp.U_NAME}/dmca')
         ]]
         reply_markup = InlineKeyboardMarkup(buttons)
         await query.message.edit_text(
