a8769a4d4480d116fe3eae220685495db214cb0e
त्रिकालदर्शी <86564011+trikaaldarshi@users.noreply.github.com>
2025-10-04 17:31:35 +0530

    Update info.py

--- info.py
+++ info.py
@@ -199,34 +199,32 @@ BAD_WORDS = {
 # ============================
 # Server & Web Configuration
 # ============================
-
-NO_PORT = bool(environ.get('NO_PORT', False))
+NO_PORT = is_enabled(environ.get('NO_PORT', 'False'), False)
 APP_NAME = None
+
 if 'DYNO' in environ:
     ON_HEROKU = True
     APP_NAME = environ.get('APP_NAME')
 else:
     ON_HEROKU = False
-BIND_ADRESS = str(getenv('WEB_SERVER_BIND_ADDRESS', '0.0.0.0'))
-FQDN = str(getenv('FQDN', BIND_ADRESS)) if not ON_HEROKU or getenv('FQDN') else APP_NAME+'.herokuapp.com'
-URL = "https://{}/".format(FQDN) if ON_HEROKU or NO_PORT else "https://{}/".format(FQDN, PORT)
-SLEEP_THRESHOLD = int(environ.get('SLEEP_THRESHOLD', '60'))
-WORKERS = int(environ.get('WORKERS', '4'))
-SESSION_NAME = str(environ.get('SESSION_NAME', 'dreamXBotz'))
-MULTI_CLIENT = False
-name = str(environ.get('name', 'DREAMXBOTZ'))
-PING_INTERVAL = int(environ.get("PING_INTERVAL", "1200"))  # 20 minutes
-if 'DYNO' in environ:
-    ON_HEROKU = True
-    APP_NAME = str(getenv('APP_NAME'))
-else:
-    ON_HEROKU = False
-HAS_SSL = bool(getenv('HAS_SSL', True))
-if HAS_SSL:
-    URL = "https://{}/".format(FQDN)
-else:
-    URL = "http://{}/".format(FQDN)
 
+BIND_ADDRESS = str(getenv('WEB_SERVER_BIND_ADDRESS', '0.0.0.0'))
+FQDN = str(getenv('FQDN', BIND_ADDRESS)) if not ON_HEROKU or getenv('FQDN') else APP_NAME+'.herokuapp.com'
+
+HAS_SSL = is_enabled(getenv('HAS_SSL', 'True'), True)
+
+if ON_HEROKU:
+    URL = "https://{}/".format(FQDN)
+elif NO_PORT:
+    if HAS_SSL:
+        URL = "https://{}/".format(FQDN)
+    else:
+        URL = "http://{}/".format(FQDN)
+else:
+    if HAS_SSL:
+        URL = "https://{}:{}/".format(FQDN, PORT)
+    else:
+        URL = "http://{}:{}/".format(FQDN, PORT)
 # ============================
 # Reactions Configuration
 # ============================
